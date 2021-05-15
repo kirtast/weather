03f790312ae46c2c7d67180d0968b7939b873318
kirtast <jpon.her@gmail.com>
2021-05-15 21:13:21 +0200

    testing localhost flask

---
+++ API_Flask_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import app_test
+
+app = Flask(__name__)
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///DB_weather.sqlite'
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def main_page():
+    conn=sqlite3.connect('DB_weather.sqlite')
+    cur=conn.cursor()
+    cur.execute('SELECT * FROM Daily')
+    rows = cur.fetchall()
+    cur.close()
+    return render_template('home.html', rows = rows)
